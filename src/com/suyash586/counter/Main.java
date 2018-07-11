577bd417e95b5b3f1d3550a5375f2fb4cbd828d9
Suyash Patil <patsuyash@gmail.com>
2018-07-12 00:22:53 +0530

    init

---
+++ src/com/suyash586/counter/Main.java
@@ -0,0 +1,20 @@
+package com.suyash586.counter;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// Using Scanner for Getting Input from User
+		Scanner in = new Scanner(System.in);
+
+		String s = in.nextLine();
+		System.out.println("You entered string " + s);
+
+		int a = in.nextInt();
+		System.out.println("You entered integer " + a);
+
+		float b = in.nextFloat();
+		System.out.println("You entered float " + b);
+	}
+}
